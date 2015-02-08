6e5e75a993d1b1669dae349828ad36165e9fea24
fieldfirst <fieldfirst2012@gmail.com>
2015-02-08 18:14:42 +0700

    Trying to fixes Windows' JTabbedpane issues

--- src/com/robotz/view/FlatTabbedPaneUI.java
+++ src/com/robotz/view/FlatTabbedPaneUI.java
@@ -6,10 +6,12 @@ import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.LayoutManager;
 import java.awt.Rectangle;
 
-import javax.swing.UIManager;
 import javax.swing.plaf.basic.BasicTabbedPaneUI;
+
+import Main.RobotMain;
 
 public class FlatTabbedPaneUI extends BasicTabbedPaneUI {
 	
@@ -27,7 +29,6 @@ public class FlatTabbedPaneUI extends BasicTabbedPaneUI {
 			g2.setColor(new Color(77, 182, 172));
 		g2.drawRect(x, y, w, h);
 		g2.fillRect(x, y, w, h);
-		
 	}
 
 	@Override
@@ -50,5 +51,21 @@ public class FlatTabbedPaneUI extends BasicTabbedPaneUI {
 		g2.setColor(Color.WHITE);
 		g2.drawString(title, textRect.x , textRect.y + metrics.getAscent());
 	}
+	
+	@Override
+    protected LayoutManager createLayoutManager() {
+		if (RobotMain.getOSName().contains("win"))
+			return new MyTabbedPaneLayout();
+		else
+			return super.createLayoutManager();
+    }
+
+    protected class MyTabbedPaneLayout extends TabbedPaneLayout {
+        @Override
+        protected void padSelectedTab(int tabPlacement, int selectedIndex) {
+            //do nothing!
+            //super.padSelectedTab(tabPlacement, selectedIndex);
+        }
+    }
 
 }
